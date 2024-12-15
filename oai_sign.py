a57834cb988116d0a6e950230fd474edaab96b78
Gurosuogen <zcw5321@gmail.com>
2024-12-15 20:56:08 +0300

    update aoi_sign.py

--- oai_sign.py
+++ oai_sign.py
@@ -2,7 +2,7 @@
 任务名称
 name: OAI签到
 定时规则
-cron: 2 8 * * *
+cron: 5 0 * * *
 """
 import requests
 import os
@@ -25,23 +25,24 @@ def reqSign():
         return
 
     headers = {
-        "authority": "oai.itsfurry.com",
+        # "authority": "oai.itsfurry.com",
         "accept": "application/json, text/plain, */*",
-        "accept-encoding": "gzip, deflate, br, zstd",
         "accept-language": "zh-CN,zh;q=0.9",
+        # "accept-encoding": "gzip, deflate, br, zstd",
         "content-type": "application/json",
         "cookie": cookie,
         "new-api-user": "3262",
-        "origin": "https://oai.itsfurry.com",
-        "referer": "https://oai.itsfurry.com/",
-        "sec-ch-ua": '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Microsoft Edge\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
         "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
+        "sec-ch-ua-platform": "\"Windows\"",
         "sec-fetch-dest": "empty",
         "sec-fetch-mode": "cors",
         "sec-fetch-site": "same-origin",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
-    }
+        # "origin": "https://oai.itsfurry.com",
+        "Referer": "https://oai.itsfurry.com/",
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+        # "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
     data = {
         "id": 3262
     }
@@ -49,14 +50,14 @@ def reqSign():
         response = requests.post(url, json=data, headers=headers)
         print("状态码:", response.status_code)
         if response.status_code == 200:
-            sendNotify("签到成功", "")
+            QLAPI.notify("签到成功", "")
         else:
-            sendNotify("签到失败", response.text)
+            QLAPI.notify("签到失败", response.text)
         print("响应内容:", response.text)
 
     except requests.exceptions.RequestException as e:
         print("请求失败:", e)
-        sendNotify("oai签到失败", str(e))
+        QLAPI.notify("oai签到失败", str(e))
 
 
 if __name__ == '__main__':
